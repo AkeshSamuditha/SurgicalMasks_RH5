b669042229da44b138c03dfa705d5e5a1cec3186
AkeshSamuditha <akesh.20@cse.mrt.ac.lk>
2024-04-21 00:54:12 +0530

    api endpoints setuped

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,12 @@ from pymongo.mongo_client import MongoClient
 
 def connect():
     uri = "mongodb+srv://surgicalmasks:6ey0eKacdG2MdmvB@cluster0.uovckvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
     client = MongoClient(uri)
     # Send a ping to confirm a successful connection
     try:
         client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
+        return client
     except Exception as e:
         print(e)
     
@@ -32,6 +32,16 @@ def intializeData():
     collection.insert_many(data_dict)
     print("Data inserted successfully")
     
+def add_csv_data(data, client):
+    # check columns are present and return if they are not same
     
-connect()
+    data.drop(columns=['eid'], inplace=True)
+    data.reset_index(drop=True, inplace=True)
+    data_dict = data.to_dict("records", into=dict)
 
+    collection = client["prognostic"]["healthWorkerData"]
+
+    #input the data into the database
+    collection.insert_many(data_dict)
+    return "Data inserted successfully"
+    
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import pandas as pd
+from io import StringIO
+from database import connect, add_csv_data
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class User(BaseModel):
+    username: str
+    password: str
+    
+class Symptoms(BaseModel):
+    bmi: float
+    
+
+@app.on_event("startup")
+async def startup_event():
+    global client
+    client = connect()
+
+@app.post("/login")
+def login(user: User):
+    if user.username != "test" or user.password != "test":
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+    return {"status": "Logged in"}
+
+@app.post("/user_data")
+def receive_medical_data(data: MedicalData):
+    # Process the medical data here
+    collection = client["prognostic"]["userMedicalData"]
+    collection.insert_one(data.dict())
+    return {"status": "Data received"}
+
+
+@app.post("/medical_worker_data")
+async def receive_medical_worker_data(file: UploadFile = File(...)):
+    if file.filename.endswith('.csv'):
+        contents = await file.read()
+        df = pd.read_csv(StringIO(contents.decode('utf-8')))
+        
+        add_csv_data(df, client)
+        return {"status": "Data received"}
+    else:
+        raise HTTPException(status_code=400, detail="Invalid file type")
