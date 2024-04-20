4dfdcf0e7b7a60d746a06a72d63da2853b409521
tharoosha <tharooshavihidun@gmail.com>
2024-04-20 23:34:34 +0530

    home page add

---
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+function HomePage() {
+  return (
+    <Grid container xs={12} className='px-5 lg:px-36 justify-between'>
+        
+    </Grid>
+
+  )
+}
+
+export default HomePage
